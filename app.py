e9f02bdde0c5e4ceb8503f23f9533e1a08055599
Larissa Alves <42353981+larissa144@users.noreply.github.com>
2021-10-13 22:28:42 -0300

    Customizando a mensagem

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
+    return "Oiii, meu nome é larissa, tenho 21 anos e sou dev front-end!"
 
 if __name__ == '__main__':
     app.run(debug=True)
