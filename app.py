a49cf7b49858ce787a4d208cc219eece11515a44
Larissa Alves <42353981+larissa144@users.noreply.github.com>
2021-11-03 19:47:56 -0300

    CSRF

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+
+csrf = CSRFProtect(app)                                                                                                                           
 
 @app.route("/")
 def pagina_inicial():
     return "Oiii, meu nome é larissa, tenho 21 anos e sou dev front-end!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
